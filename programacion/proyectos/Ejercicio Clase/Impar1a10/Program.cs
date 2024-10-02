d1f8412d2bc161b94b87d9b8aba3aafebe66ab22
Javi Escobar Fernandez <javi.escobar.fernandez@gmail.com>
2024-10-02 20:06:42 +0200

    Ejercicio Clase 1 Mostrar numeros impares del 1 al 10

---
+++ programacion/proyectos/Ejercicio Clase/Impar1a10/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace Impar1a10
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Funcion para mostrar los numeros impares del 1 al 10.
+            UInt32 x = 1;
+            UInt32 resultado = x;
+            while (resultado < 10)
+            {
+                Console.WriteLine(resultado);
+                x++;
+                resultado = x * 2 - 1;
+            }
+        }
+    }
+}
