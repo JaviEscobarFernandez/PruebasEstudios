015711238f14ce5488eb9acbca19689de2936e6f
Javi Escobar Fernandez <javi.escobar.fernandez@gmail.com>
2024-10-11 21:59:26 +0200

    :rocket: Ejercicio Clase: suma del 1 al 5

---
+++ programacion/proyectos/Ejercicio Clase/Suma1a5/codigo/Prueba/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Prueba
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 1;
+            int result = 0;
+            while (i <= 5)
+            {
+                result += i;
+                i++;
+            }
+            Console.WriteLine(result);
+        }
+    }
+}
