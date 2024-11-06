77a4f636b730a66016695ebaf852c39b8c431770
Javi Escobar Fernandez <javi.escobar.fernandez@gmail.com>
2024-11-06 19:49:57 +0100

    :rocket: Actualizado ejercicio empleados salarios resuelto en clase

--- programacion/proyectos/Ejercicio Clase/nombres_5_empleados/Program.cs
+++ programacion/proyectos/Ejercicio Clase/nombres_5_empleados/Program.cs
@@ -6,6 +6,45 @@ namespace nombres_5_empleados
     {
         static void Main(string[] args)
         {
+            // ejercicioPlanteado(); // Lo he planteado con una matriz en lugar de dos vectores
+            ejercicioResueltoEnClase(); // Aqui se ha planteado con dos vectores
+        }
+
+        public static void ejercicioResueltoEnClase()
+        {
+            int numeroEmpleados = 5;
+            string[] nombreEmpleado = new string[numeroEmpleados];
+            double[] salarioEmpleado = new double[numeroEmpleados];
+
+            // Paso 01: Pedir los datos:
+            for (int i = 0; i < nombreEmpleado.Length; i++)
+            {
+                Console.Write($"Por favor, introduce el nombre del empleado {i + 1}: ");
+                nombreEmpleado[i] = Console.ReadLine();
+                double salario = 0;
+                do
+                {
+                    Console.Write("Ahora introduce su salario: ");
+                    salario = double.Parse(Console.ReadLine());
+                }
+                while (salario <= 0);
+                salarioEmpleado[i] = salario;
+            }
+
+            // Paso 02: Comprobar el empleado con el mayor salario:
+            int posicionSueldoMayor = 0;
+            for (int j = 0; j < nombreEmpleado.Length; j++)
+            {
+                if (salarioEmpleado[j] > salarioEmpleado[posicionSueldoMayor])
+                    posicionSueldoMayor = j;
+            }
+
+            // Paso 03: Mostrar empleado y sueldo correspondiente
+            Console.WriteLine($"El empleado con mayor sueldo es: {nombreEmpleado[posicionSueldoMayor]} con un sueldo de: {salarioEmpleado[posicionSueldoMayor]}");
+        }
+
+        public static void ejercicioPlanteado()
+        {
             // Programa que permita ingresar los nombres de 5 empleados y sus respectivos sueldos.
             // El programa debe mostrar el sueldo mayor y el nombre del empleado al que pertenece.
             // Los sueldos son tipo double
