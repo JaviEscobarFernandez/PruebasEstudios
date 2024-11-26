f6e866eb9dfd6f591dcb879f69f7a0fcd9f8d2bb
Javi Escobar Fernandez <javi.escobar.fernandez@gmail.com>
2024-11-26 10:29:32 +0100

    :rocket: Programación: Subido ejercicio Videotutoria 8 - crear libreria propia y realizar operaciones llamando a la libreria.

---
+++ programacion/proyectos/Ejercicio Clase/videotutoria8/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using libreria_vt08;
+
+namespace videotutoria_8
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1 = 10;
+            int num2 = 2;
+            Console.WriteLine($"La suma de {num1} + {num2} es: {Operaciones.Sumar(num1, num2)}");
+            Console.WriteLine($"La resta de {num1} - {num2} es: {Operaciones.Restar(num1, num2)}");
+            Console.WriteLine($"La multiplicación de {num1} * {num2} es: {Operaciones.Multiplicar(num1, num2)}");
+            Console.WriteLine($"La división de {num1} / {num2} es: {Operaciones.Dividir(num1, num2)}");
+        }
+    }
+}
---
+++ programacion/proyectos/Ejercicio Clase/videotutoria8/libreria_vt08/Operaciones.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace libreria_vt08
+{
+    public class Operaciones
+    {
+        public static int Sumar(int num1, int num2) {  return num1 + num2; }
+        public static int Restar(int num1, int num2) { return num1 - num2; }
+        public static int Multiplicar(int num1, int num2) { return num1 * num2; }
+        public static int Dividir(int num1, int num2)
+        {
+            if (num2 == 0)
+            {
+                Console.WriteLine("ERROR: no es posible dividir entre 0");
+                return 0;
+            }
+            return num1 / num2;
+        }
+    }
+}
