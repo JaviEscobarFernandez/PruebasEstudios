43c0e6e9dc6f2a433c080f8de9bfa86b4e824b93
Javi Escobar Fernandez <javi.escobar.fernandez@gmail.com>
2024-12-18 19:11:03 +0100

    :rocket: Programación: Subida actualización ejercicio Videotutoria 1 - numeros impares 1 a 10.

---
+++ programacion/proyectos/Ejercicio Clase/Impar1a10/codigo/actualizacion/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace impares_1_10
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Mostrar números impares del 1 al 10:");
+            for (int i = 0; i < 10; i++)
+            {
+                if (i % 2 != 0) { Console.WriteLine(i); }
+            }
+        }
+    }
+}
