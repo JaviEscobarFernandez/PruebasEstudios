552711e8048ed41967c594aaad9bc08ab431dc19
Javi Escobar Fernandez <javi.escobar.fernandez@gmail.com>
2024-12-18 19:25:56 +0100

    :rocket: Programación: Subido ejercicio Videotutoria 1 - comprobar número mayor

---
+++ programacion/proyectos/Ejercicio Clase/comprobar_numero_mayor/codigo/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace comprobar_numero_mayor
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hola, vamos a comprobar si el primer numero es mayor al segundo.");
+            int x = SolicitarNumero();
+            int y = SolicitarNumero();
+            CompararNumeros(x, y);
+        }
+
+        static int SolicitarNumero()
+        {
+            int num = -1;
+            do
+            {
+                Console.Write("Introduce el número: ");
+                string dato = Console.ReadLine();
+                num = ValidarNumero(dato);
+            }
+            while (num < 0);
+
+            return num;
+        }
+
+        static int ValidarNumero(string dato)
+        {
+            int num = 0;
+            if (int.TryParse(dato, out num) && num >= 0)
+                return num;
+
+            Console.WriteLine("ERROR: Dato introducido no válido, debe ser un número positivo.");
+            return -1;
+        }
+
+        static void CompararNumeros(int x, int y)
+        {
+            if (x > y)
+                Console.WriteLine($"{x} es mayor que {y}");
+            else if (x == y)
+                Console.WriteLine($"{x} es igual que {y}");
+            else
+                Console.WriteLine($"{x} es menor que {y}");
+        }
+    }
+}
