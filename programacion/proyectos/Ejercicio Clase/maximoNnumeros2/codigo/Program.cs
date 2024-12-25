0ca6f73ca9c6f3c49a174d2658d07ac96d9cfaf5
Javi Escobar Fernandez <javi.escobar.fernandez@gmail.com>
2024-12-25 22:15:02 +0100

    :rocket: Programación: Subido ejercicio clase Máximo n números (version mejorada)

---
+++ programacion/proyectos/Ejercicio Clase/maximoNnumeros2/codigo/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace maximoNNumeros2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int max = 0;
+            int numero = 0;
+            Console.WriteLine("Hola, vamos a calcular el máximo de N numeros.");
+            do
+            {
+                Console.Write("Introduce un número (negativo para finalizar): ");
+                numero = ValidarNumero(Console.ReadLine());
+                if (numero > max)
+                    max = numero;
+            }
+            while (numero >= 0);
+            Console.WriteLine($"El número máximo es: {max}");
+        }
+
+        static int ValidarNumero(string datos)
+        {
+            int numero = 0;
+            if (!int.TryParse(datos, out numero))
+            {
+                Console.WriteLine("ERROR: dato introducido no válido.");
+                numero = 0;
+            }
+            return numero;
+        }
+    }
+}
