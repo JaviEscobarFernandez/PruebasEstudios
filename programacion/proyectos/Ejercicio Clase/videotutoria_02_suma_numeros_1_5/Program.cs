dd5ab42a026f5d33b718535168d347a00b669809
Javi Escobar Fernandez <javi.escobar.fernandez@gmail.com>
2024-12-25 12:53:05 +0100

    :rocket: Programación: Subido ejercicio videotutoria 02, suma numeros 1 a 5 + tabla de pensamiento computacional

---
+++ programacion/proyectos/Ejercicio Clase/videotutoria_02_suma_numeros_1_5/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace videotutoria_02_suma_numeros_1_5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Realiza un programa que sume los números del 1 al 5. Debes emplear un bucle.
+            // Después, comprueba su funcionamiento con una tabla de pensamiento computacional.
+
+            Console.WriteLine("Tabla de pensamiento computacional:");
+            Console.WriteLine("----------------------");
+            Console.WriteLine("|   cont  |   suma   |");
+            Console.WriteLine("----------------------");
+            int suma = 0;
+            for (int cont = 0; cont <= 5; cont++)
+            {
+                suma += cont;
+                // Simplemente para visualizar mejor el resultado en la tabla
+                string sumaResult = suma < 10 ? " " + suma.ToString() : suma.ToString();
+                Console.WriteLine($"| cont: {cont} | suma: {sumaResult} |");
+            }
+            Console.WriteLine("----------------------");
+            Console.WriteLine($"El resultado es: {suma}");
+        }
+    }
+}
