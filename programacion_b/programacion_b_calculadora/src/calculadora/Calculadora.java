5dbdcdd3ec3688666189cd77797989759c42137f
Javi Escobar Fernandez <javi.escobar.fernandez@gmail.com>
2025-03-07 20:05:55 +0100

    :rocket: Programación: Subido ejercicio Videotutoria 03 Calculadora

---
+++ programacion_b/programacion_b_calculadora/src/calculadora/Calculadora.java
@@ -0,0 +1,38 @@
+package calculadora;
+
+public class Calculadora {
+	private static int numOperaciones;
+
+	public Calculadora() {
+		numOperaciones = 0;
+	}
+
+	private void getNumOperaciones() {
+		System.out.println("La calculadora ha realizado: " + numOperaciones + " operaciones.");
+	}
+
+	private void incrementNumOperaciones() {
+		++numOperaciones;
+		getNumOperaciones();
+	}
+
+	public void operacion(int num1, int num2) {
+		System.out.println("La multiplicación de: " + num1 + " y " + num2 + " es: " + (num1 * num2));
+		incrementNumOperaciones();
+	}
+
+	public void operacion(int num1, int num2, String o) {
+		String op = o.toLowerCase();
+		if (op.equals("sumar")) {
+			System.out.println("La suma de: " + num1 + " y " + num2 + " es: " + (num1 + num2));
+			incrementNumOperaciones();
+		}
+		else if (op.equals("restar")) {
+			System.out.println("La resta de: " + num1 + " y " + num2 + " es: " + (num1 - num2));
+			incrementNumOperaciones();
+		}
+		else {
+			System.out.println("Error, operacion no válida");
+		}
+	}
+}
---
+++ programacion_b/programacion_b_calculadora/src/calculadora/Main.java
@@ -0,0 +1,22 @@
+package calculadora;
+
+/*
+ * Ejercicio propuesto en Videotutoria 03
+ * Realizar una calculadora, con su clase, instanciarla y realizar unas operaciones.
+ * Un solo atributo llamado numOperaciones que se incremente con cada operacion realizada.
+ * Crear un metodo operacion que reciba dos parametros enteros y muestro su multiplicacion
+ * Crear un metodo operacion que reciba tres parametros, dos numeros enteros y un String
+ * Si el String es "sumar", que muestre la suma de los enteros
+ * Si el String es "restar", que muestre la resta de los enteros
+ * Tras cada operacion realizada, incrementar en 1 el valor de numOperaciones y mostrar el resultado y la cuenta de numOperaciones.
+ */
+public class Main {
+
+	public static void main(String[] args) {
+		Calculadora calculadora = new Calculadora();
+		calculadora.operacion(1, 4);
+		calculadora.operacion(1, 8, "sumar");
+		calculadora.operacion(10, 2, "RESTAR");
+		calculadora.operacion(3, 40, "Redondear");
+	}
+}
