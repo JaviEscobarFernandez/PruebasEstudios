c759d265ed2eb412e27a86a8fa6dd1644fff7c30
Javi Escobar Fernandez <javi.escobar.fernandez@gmail.com>
2025-06-29 22:34:17 +0200

    :rocket: Python: Subidos ejercicios Iniciales del día 1 de Python TOTAL

---
+++ python/curso_python_total/dia1/Cerveceria.py
@@ -0,0 +1,4 @@
+# Crear un programa que pida dos nombres y los combine para formar un nombre entre los resultados introducidos
+
+print("La cerveza se llama\n\"" + input("Introduce el nombre de una mascota: ") + " " +
+      input("Introduce el nombre de tu equipo de futbol: ") + "\"")
---
+++ python/curso_python_total/dia1/primeros_pasos.py
@@ -0,0 +1,32 @@
+# Practicas y ejercicios del día 1:
+
+print("Hola mundo te digo 'Hola'")
+print(100)
+
+print(100 + 50)
+
+print("Estudiar \"Python\" es genial")
+
+print(5 + (5 * 11 * 10))
+
+print('100 + 50')
+
+print("Hola" + " " + 'Javi')
+
+print("Esta es una linea\nY esta es otra linea")
+print("Y esta es la tercera linea")
+
+print("Probando\ttabulacion")
+
+print('this isn\'t a number')
+print("this isn't a number")
+
+print("Este signo '\\' es una barra invertida")
+
+print("Línea 1\nLínea 2\nLínea 3")
+
+print("A\tB\tC\nD\tE\tF\nG\tH\tI")
+
+print("Barra Normal: /\nBarra Invertida: \\")
+
+print("Tu nombre es: " + input("Dime tu nombre: ") + " " + input("Dime tu apellido: "))
