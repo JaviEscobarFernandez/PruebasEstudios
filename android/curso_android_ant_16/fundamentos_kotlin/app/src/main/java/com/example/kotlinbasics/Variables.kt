70f190d2823731d26859ae130a9813c3774293da
Javi Escobar Fernandez <javi.escobar.fernandez@gmail.com>
2025-09-29 20:05:40 +0200

    :rocket: Kotlin: subido primeras clases de fundamentos kotlin del curso ANT kotlin 16

---
+++ android/curso_android_ant_16/fundamentos_kotlin/app/src/main/java/com/example/kotlinbasics/Variables.kt
@@ -0,0 +1,22 @@
+package com.example.kotlinbasics
+
+fun main() {
+    // Prueba variables basica
+    print("Test")
+    print("\n") // salto de linea
+
+    var lenguaje = "Kotlin"
+    val plataforma = "Android"
+    println(lenguaje)
+    println(plataforma)
+
+    // Probando constantes
+    val lugar_nacimiento = "Xirivella" // constante
+    // lugar_nacimiento = "algo"
+    println(lugar_nacimiento)
+
+    // probando variables
+    var edad = 32 // variable
+    edad = 33
+    println("$edad  años.")
+}
