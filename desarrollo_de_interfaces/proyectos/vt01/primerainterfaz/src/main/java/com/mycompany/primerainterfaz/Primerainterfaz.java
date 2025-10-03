a77f624d4653f46295b40e4e405684aacadf809e
Javi Escobar Fernandez <javi.escobar.fernandez@gmail.com>
2025-10-03 09:50:41 +0200

    :rocket: Desarrollo de Interfaces: Subido prueba de la VT01

---
+++ desarrollo_de_interfaces/proyectos/vt01/primerainterfaz/src/main/java/com/mycompany/primerainterfaz/Primerainterfaz.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.primerainterfaz;
+
+/**
+ *
+ * @author javie
+ */
+public class Primerainterfaz {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
