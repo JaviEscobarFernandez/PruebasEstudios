814a001f470aacbabdfa9d53cdb85f4304274085
Javi Escobar Fernandez <javi.escobar.fernandez@gmail.com>
2025-10-13 15:24:06 +0200

    :rocket: Acceso a datos - Java - Clase File: Subido Ejercicio 3 Variante VT01

--- acceso_a_datos/vt01/videotutoria01/src/vt01/Ejercicio3.java
+++ acceso_a_datos/vt01/videotutoria01/src/vt01/Ejercicio3.java
@@ -8,13 +8,15 @@ import java.io.IOException;
  * Ejercicio 3: Crea un programa en Java que a partir de una cadena de caracteres,
  * permita almacenarlos en un fichero de texto
  * El programa añadirá un caracter * para indicar que ha llegado al final del fichero.
+ *
+ * Variante: que almacene Strings a partir de un array de Strings
  */
 public class Ejercicio3 {
 
 	public static void main(String[] args) {
-		String texto = "Cadena de caracteres a añadir al fichero";
 		File directorio = new File("Ejercicio3");
 		File fichero = new File(directorio, "fichero.txt");
+		File fichero2 = new File(directorio, "fichero2.txt");
 
 		try {
 			directorio.mkdir();
@@ -23,6 +25,12 @@ public class Ejercicio3 {
 			System.err.println(e.toString());
 		}
 
+		varianteUno(fichero);
+		varianteDos(fichero2);
+	}
+
+	private static void varianteUno(File fichero) {
+		String texto = "Cadena de caracteres a añadir al fichero";
 		try {
 			// Abrimos el flujo al fichero
 			FileWriter fw = new FileWriter(fichero);
@@ -33,6 +41,28 @@ public class Ejercicio3 {
 			char[] cadena = texto.toCharArray();
 			for (char c : cadena) {
 				fw.write(c);
+			}
+			// Añadimos el caracter * al final del fichero
+			fw.append('*');
+			// Aplicamos los cambios en el fichero
+			fw.flush();
+
+			// Cerramos el flujo
+			fw.close();
+		} catch (IOException e) {
+			System.err.println(e.toString());
+		}
+	}
+
+	private static void varianteDos(File fichero) {
+		String[] textos = { "Texto array 1", "Texto array 2", "texto array 3", "Texto array 4" };
+		try {
+			// Abrimos el flujo al fichero
+			FileWriter fw = new FileWriter(fichero);
+
+			// Escribimos en el fichero
+			for (String t : textos) {
+				fw.write(t);
 			}
 			// Añadimos el caracter * al final del fichero
 			fw.append('*');
