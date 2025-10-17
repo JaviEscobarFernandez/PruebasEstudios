35a8c1a4271a1056ba30ac151fdac6c6d7f46e14
Javi Escobar Fernandez <javi.escobar.fernandez@gmail.com>
2025-10-17 15:05:49 +0200

    :rocket: Desarrollo de Interfaces: Subido Ejercicios de la VT02

---
+++ desarrollo_de_interfaces/proyectos/vt02/videotutoria2_ejemplos/src/main/java/com/mycompany/videotutoria2_ejemplos/Videotutoria2_ejemplos.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.videotutoria2_ejemplos;
+
+/**
+ *
+ * @author javie
+ */
+public class Videotutoria2_ejemplos {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
