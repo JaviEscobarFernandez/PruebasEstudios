d6cecbc7c9547723f74c8ccdbc991ae640646785
Javi Escobar Fernandez <javi.escobar.fernandez@gmail.com>
2025-10-17 09:52:40 +0200

    :rocket: Desarrollo de Interfaces: Subido Ejercicio VT01 prueba interfaz simple

---
+++ desarrollo_de_interfaces/proyectos/vt01/interfaz_prueba/src/main/java/com/mycompany/interfaz_prueba/Interfaz_prueba.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.interfaz_prueba;
+
+/**
+ *
+ * @author javie
+ */
+public class Interfaz_prueba {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
