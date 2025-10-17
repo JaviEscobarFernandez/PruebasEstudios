f112b2e2093c3d3eb60faf86fd6f93055f65983b
Javi Escobar Fernandez <javi.escobar.fernandez@gmail.com>
2025-10-17 10:15:28 +0200

    :rocket: Desarrollo de Interfaces: Subido Ejercicio1 propuesto VT02

---
+++ desarrollo_de_interfaces/proyectos/vt02/ejercicio1_propuesto/src/main/java/com/mycompany/ejercicio1_propuesto/Ejercicio1_propuesto.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.ejercicio1_propuesto;
+
+/**
+ *
+ * @author javie
+ */
+public class Ejercicio1_propuesto {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
